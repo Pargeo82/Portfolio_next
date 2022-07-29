d853f0fa5bcc52daf76ae717be86383dc29860b1
Pargeo <pargeo82@gmail.com>
2022-07-29 23:49:21 +0200

    add nav link highlight

--- components/navigation.js
+++ components/navigation.js
@@ -1,20 +1,38 @@
 import Link from "next/link";
 import styles from "./navigation.module.css";
+import { useRouter } from "next/router";
 
 export default function Navigation() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
     <nav className={styles.nav}>
       <h2 className={styles.links}>
-        <Link href="/">Home</Link>
+        <Link href="/">
+          <a className={currentRoute === "/" ? `${styles.active}` : `${styles.nonActive}`}>Home</a>
+        </Link>
       </h2>
       <h2 className={styles.links}>
-        <Link href="/projects">Projects</Link>
+        <Link href="/projects">
+          <a className={currentRoute === "/projects" ? `${styles.active}` : `${styles.nonActive}`}>
+            Projects
+          </a>
+        </Link>
       </h2>
       <h2 className={styles.links}>
-        <Link href="/hobbies">Hobbies</Link>
+        <Link href="/hobbies">
+          <a className={currentRoute === "/hobbies" ? `${styles.active}` : `${styles.nonActive}`}>
+            Hobbies
+          </a>
+        </Link>
       </h2>
       <h2 className={styles.links}>
-        <Link href="/contact">Contact</Link>
+        <Link href="/contact">
+          <a className={currentRoute === "/contact" ? `${styles.active}` : `${styles.nonActive}`}>
+            Contact
+          </a>
+        </Link>
       </h2>
     </nav>
   );
