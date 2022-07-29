8e26b6a060a027dae68edc51c19e44fdf6940a27
Pargeo82 <pargeo82@gmail.com>
2022-07-29 11:27:53 +0200

    add social icons and links

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
 import Navigation from "../components/navigation";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Navigation />
-      <h1>I'm Pargeo</h1>
+      <div className="container">
+        <div className="left">
+          <h1>
+            Hi, I'm <span id="Mislav">Mislav Markušić</span>
+          </h1>
+          <h2>
+            also known as <span id="Parg">Pargeo</span>
+          </h2>
+          <div id="follow">
+            <p>Find me on</p>
+            <a
+              href="https://www.linkedin.com/in/mislav-markusic/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                className="icon"
+                src="/icons8-linkedin.svg"
+                alt="linkedin"
+                width={30}
+                height={30}
+              />
+            </a>
+
+            <a
+              href="https://twitter.com/MislavMarkusic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/icons8-twitter.svg"
+                alt="twitter"
+                width={30}
+                height={30}
+              />
+            </a>
+            <a
+              href="https://github.com/Pargeo82"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/icons8-github.svg"
+                alt="github"
+                width={30}
+                height={30}
+              />
+            </a>
+          </div>
+        </div>
+        <div className="right">
+          <Image src="/Pargeo.jpg" alt="me" width={375} height={518} />
+        </div>
+      </div>
     </>
   );
 }
