d646eb281af987bf283237570537fac80336a312
Pargeo82 <pargeo82@gmail.com>
2022-08-18 20:49:58 +0200

    add github calendar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Navigation from "../components/navigation";
 import Home from "../components/Home/home";
 import Skills from "../components/skills";
+import GitHubCalendar from "react-github-calendar";
 
 export default function Homepage() {
   return (
@@ -9,6 +10,7 @@ export default function Homepage() {
       <div className="container flex">
         <Home />
         <Skills />
+        <GitHubCalendar username="pargeo82" weekStart={1} />
       </div>
     </>
   );
