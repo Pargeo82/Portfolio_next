5cb28f9d2a36e47baf5f0c470881277d5f91b773
Pargeo <pargeo82@gmail.com>
2022-08-22 22:27:56 +0200

    add new and delete old resume

--- components/download_button.js
+++ components/download_button.js
@@ -3,14 +3,9 @@ import Image from "next/image";
 
 export default function DownloadButton() {
   return (
-    <a href="./CV_Mislav_Markusic.doc" download>
+    <a href="./Resume_Mislav_Markusic.pdf" download>
       <button className={styles.button}>
-        <Image
-          src="/download-svgrepo-com.svg"
-          alt="email"
-          width={30}
-          height={30}
-        />
+        <Image src="/download-svgrepo-com.svg" alt="email" width={30} height={30} />
         <span className={styles.resume}>Resume</span>
       </button>
     </a>
