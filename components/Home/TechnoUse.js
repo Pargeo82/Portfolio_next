8f1970f26e2a067560473255459f171f0d4ab253
Pargeo <pargeo82@gmail.com>
2022-08-26 19:42:15 +0200

    add media queries for mobile

--- components/Home/TechnoUse.js
+++ components/Home/TechnoUse.js
@@ -4,8 +4,14 @@ import styles from "./home.module.css";
 export default function TechnoUse(props) {
   return (
     <>
-      <div className={`flex ${styles.mediaFlex}`}>
-        <Image src={`/${props.name}.png`} alt="{props.name}" width={60} height={40} />
+      <div className={styles.mediaFlex}>
+        <Image
+          src={`/${props.name}.png`}
+          alt="{props.name}"
+          width={50}
+          height={50}
+          layout="fixed"
+        />
         <h3>{props.name}</h3>
       </div>
     </>
