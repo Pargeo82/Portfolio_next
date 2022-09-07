f6fcf3bfe7efe4c5c5e295ece126def182c75103
Pargeo82 <pargeo82@gmail.com>
2022-09-07 20:37:40 +0200

    add meta description and google verify file

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Pargeo portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Portfolio page for a junior developer Mislav Markušić. Technologies used: HTML, CSS, JS, React, NextJS, MongoDB etc. "
+        />
       </Head>
       <Component {...pageProps} />
     </>
