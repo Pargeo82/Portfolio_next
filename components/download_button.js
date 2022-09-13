c932544268dc8fa1a737a6fdd00172eff38d1ba4
Pargeo <pargeo82@gmail.com>
2022-09-13 14:57:38 +0200

    fix bug on homepage email and resume buttons

--- components/download_button.js
+++ components/download_button.js
@@ -1,11 +1,18 @@
 import styles from "./download_button.module.css";
 import Image from "next/image";
-
+// "./Resume_Mislav_Markusic.pdf download";
 export default function DownloadButton() {
   return (
-    <button className={styles.button} onClick="./Resume_Mislav_Markusic.pdf" download>
+    <a
+      className={styles.button}
+      href="./Resume_Mislav_Markusic.pdf"
+      download
+      target="_blank"
+      rel="noreferrer"
+      title="download Resume"
+    >
       <Image src="/download-svgrepo-com.svg" alt="email" width={30} height={30} />
       <span className={styles.resume}>Resume</span>
-    </button>
+    </a>
   );
 }
--- components/email_button.js
+++ components/email_button.js
@@ -1,9 +1,10 @@
+import Router, { useRouter } from "next/router";
 import styles from "./email_button.module.css";
 import Image from "next/image";
 
 export default function EmailButton() {
   return (
-    <button className={styles.button} onClick="mailto:pargeo82@gmail.com">
+    <button className={styles.button} onClick={() => Router.push("mailto:pargeo82@gmail.com")}>
       <Image src="/email20.svg" alt="email" width={30} height={30} />
       <span className={styles.email}>Email</span>
     </button>
