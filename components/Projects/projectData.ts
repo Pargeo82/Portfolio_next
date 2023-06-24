ce9a85e4b95a79b1e1ee3eea714c81148064b79e
Mislav-Markusic <ai_006@atmc.ai>
2023-06-24 17:58:21 +0200

    fix node version

--- components/Projects/projectData.ts
+++ components/Projects/projectData.ts
@@ -11,7 +11,8 @@ const projectData = [
     link: 'cedeterija',
     outsideLink: false,
     imageArray: ['/projects/cedeterija2.png', '/projects/cedeterija3.png'],
-    longerDescription: `At previous job a problem I wanted to resolve was when receiving new albums that we've never had before, there is a lot of data required to manually input. POS system is somewhat old and it requires .csv file input to populate the data, and a really long column table for it. I realized I could fetch data from discogs (which is maybe the biggest online music data library and marketplace), and use that data to populate the table and the file to import in POS`,
+    longerDescription:
+      "At previous job a problem I wanted to resolve was when receiving new albums that we've never had before, there is a lot of data required to manually input. POS system is somewhat old and it requires .csv file input to populate the data, and a really long column table for it. I realized I could fetch data from discogs (which is maybe the biggest online music data library and marketplace), and use that data to populate the table and the file to import in POS",
     linkTitle: 'Cedeterija app',
     url: 'https://cedeterija.vercel.app/',
     gitUrl: 'https://github.com/Pargeo82/cedeterija',
@@ -26,7 +27,8 @@ const projectData = [
     link: 'gt',
     outsideLink: false,
     imageArray: ['/projects/gt1.png', '/projects/gt2.png'],
-    longerDescription: `This is an online bar manu for a bar in Zaprešić. It has a dashboard where admins (me and the bar owner) have CRUD capabilites for articles, and the "special offer" at the beginning of the page. It has been written in NextJS and database is MongoDB (Atlas). It is mobile-first site.`,
+    longerDescription:
+      "This is an online bar manu for a bar in Zaprešić. It has a dashboard where admins (me and the bar owner) have CRUD capabilites for articles, and the 'special offer' at the beginning of the page. It has been written in NextJS and database is MongoDB (Atlas). It is mobile-first site.",
     linkTitle: 'Caffe Bar GT',
     url: 'https://www.caffebargt-martinko.hr/',
   },
@@ -40,7 +42,8 @@ const projectData = [
     link: 'portfolio',
     outsideLink: false,
     imageArray: ['/projects/portfolio.jpg', '/projects/portfolio2.png'],
-    longerDescription: `This is a project that had many iterations. I started it in vanilla JS, but as the time passed by it got remade and remade. This is the look I'm currently most satisfied with, not too complex, but clean. NextJS got picked because it has great SEO from the box. I have also spent significant amount of time on making google crawler work on it, did a lot of accessibility tests and speed tests, so it would get as optimized as possible. After a while I have decided to add typescript to the project, and Sass to make it more robust and easier to maintain. MaterialUI was the next step in the iteration`,
+    longerDescription:
+      "This is a project that had many iterations. I started it in vanilla JS, but as the time passed by it got remade and remade. This is the look I'm currently most satisfied with, not too complex, but clean. NextJS got picked because it has great SEO from the box. I have also spent significant amount of time on making google crawler work on it, did a lot of accessibility tests and speed tests, so it would get as optimized as possible. After a while I have decided to add typescript to the project, and Sass to make it more robust and easier to maintain. MaterialUI was the next step in the iteration",
     gitUrl: 'https://github.com/Pargeo82/Portfolio_next',
   },
 ];
