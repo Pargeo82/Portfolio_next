40678eeaec7730da1c39fba222c280f418953c90
Mislav-Markusic <ai_006@atmc.ai>
2023-11-20 18:56:33 +0100

    add head to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,18 @@ import Skills from '../components/Home/Skills';
 import Work from '../components/Home/Work';
 import Hero from '../components/Home/Hero';
 import TechnologiesUsed from '../components/Home/TechnologiesUsed';
+import Head from 'next/head';
 
 export default function Homepage() {
   return (
     <>
+      <Head>
+        <title>Pargeo Portfolio</title>
+        <meta
+          name='description'
+          content='Portfolio page for a junior developer Mislav Markušić. Technologies used: HTML, CSS, JS, React, NextJS, MongoDB'
+        />
+      </Head>
       <Hero />
       <Skills />
       <TechnologiesUsed />
