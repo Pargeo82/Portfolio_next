9b11fb9ea6f43010118143f78ecd09fcf1d58782
Pargeo82 <pargeo82@gmail.com>
2024-02-09 16:18:37 +0100

    added mai project

---
+++ pages/projects/mai.tsx
@@ -0,0 +1,114 @@
+import Image from 'next/image';
+import { Typography, Grid, Stack } from '@mui/material';
+import { useTheme } from '@mui/material';
+import projectData, { Project } from '../../components/Projects/projectData';
+import ProjectTechnologies from '../../components/technologies/ProjectTechnologies';
+import { TechnoTypes } from '../../types/technoTypes';
+import { GitHub } from '@mui/icons-material';
+import mai2 from '../../public/projects/mai2.png';
+import mai3 from '../../public/projects/mai3.png';
+
+export default function Mai() {
+  const theme = useTheme();
+  const primaryText = theme.palette.text.primary;
+  const project: Project | undefined = projectData.find((project) => project.name === 'Mai beauty salon');
+  if (project) {
+    project.imageArray = [mai2, mai3];
+  }
+  return (
+    <>
+      <Typography
+        variant='h2'
+        gutterBottom
+      >
+        {project?.title}
+      </Typography>
+      <Grid
+        container
+        spacing={4}
+        mb={8}
+      >
+        {project?.imageArray?.map((image, index) => {
+          return (
+            <Grid
+              item
+              xs={12}
+              md={6}
+              key={index}
+              alignItems='flex-start'
+              sx={{ display: 'flex' }}
+            >
+              <Image
+                key={index}
+                src={image}
+                alt={project.name}
+                width={560}
+                height={280}
+                style={{ objectFit: 'contain', width: '100%', height: 'auto', alignSelf: 'flex-start' }}
+                sizes='50vw'
+                placeholder='blur'
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Typography
+        variant='h4'
+        gutterBottom
+      >
+        Technologies used:
+      </Typography>
+      <ProjectTechnologies
+        technologies={project?.technology as TechnoTypes[]}
+        sx={{ marginBottom: 8 }}
+      />
+      <Typography
+        variant='h4'
+        gutterBottom
+      >
+        Description:
+      </Typography>
+      <Typography
+        variant='body1'
+        mb={8}
+        textAlign={'justify'}
+      >
+        {project?.longerDescription}
+      </Typography>
+      <Typography
+        variant='h4'
+        gutterBottom
+      >
+        Links:
+      </Typography>
+      <Stack mb={12}>
+        {project?.url && (
+          <a
+            href={project?.url}
+            target='_blank'
+            rel='noreferrer noopener'
+            style={{ color: primaryText }}
+          >
+            <Typography
+              variant='body1'
+              gutterBottom
+            >
+              {project.linkTitle}
+            </Typography>
+          </a>
+        )}
+        {project?.gitUrl && (
+          <a
+            href={project?.gitUrl}
+            target='_blank'
+            rel='noreferrer noopener'
+            title='github link'
+            style={{ color: primaryText }}
+          >
+            <GitHub sx={{ fontSize: 40 }} />
+          </a>
+        )}
+      </Stack>
+    </>
+  );
+}
