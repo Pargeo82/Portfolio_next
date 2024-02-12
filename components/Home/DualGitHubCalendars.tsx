1d2d14aedbce9c83482fb650273c9fba214a8f22
Pargeo82 <pargeo82@gmail.com>
2024-02-12 17:48:18 +0100

    dual git calendar description

--- components/Home/DualGitHubCalendars.tsx
+++ components/Home/DualGitHubCalendars.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 
@@ -7,57 +7,66 @@ const DualGitHubCalendars = () => {
   const [totalContributions2, setTotalContributions2] = useState<number>(0);
 
   return (
-    <Box sx={{ position: 'relative', height: '200px', mb: 4 }}>
-      <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }}>
-        <GitHubCalendar
-          username='Mislav-Markusic'
-          blockSize={18}
-          blockMargin={4}
-          theme={{
-            level4: '#39d353',
-            level3: '#26a641',
-            level2: '#006d32',
-            level1: '#0e4429',
-            level0: 'transparent',
-          }}
-          transformData={(data) => {
-            let calendarContributions = 0;
-            data.forEach((day) => {
-              calendarContributions += day.count;
-            });
-            setTotalContributions1(calendarContributions);
-            return data;
-          }}
-          weekStart={1}
-          transformTotalCount
-          totalCount={totalContributions1 + totalContributions2}
-        />
-      </div>
-      <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 0 }}>
-        <GitHubCalendar
-          username='Pargeo82'
-          blockSize={18}
-          blockMargin={4}
-          weekStart={1}
-          theme={{
-            level4: '#39d353',
-            level3: '#26a641',
-            level2: '#006d32',
-            level1: '#0e4429',
-            level0: '#262626',
-          }}
-          transformData={(data) => {
-            let calendarContributions = 0;
-            data.forEach((day) => {
-              calendarContributions += day.count;
-            });
-            setTotalContributions2(calendarContributions);
-            return data;
-          }}
-          hideTotalCount={true}
-        />
-      </div>
-    </Box>
+    <>
+      <Typography
+        variant='body2'
+        gutterBottom
+      >
+        Combined work and private calendar
+      </Typography>
+      <Box sx={{ position: 'relative', height: '200px', mb: 4 }}>
+        <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }}>
+          <GitHubCalendar
+            username='Mislav-Markusic'
+            blockSize={18}
+            blockMargin={4}
+            theme={{
+              level4: '#39d353',
+              level3: '#26a641',
+              level2: '#006d32',
+              level1: '#0e4429',
+              level0: 'transparent',
+            }}
+            transformData={(data) => {
+              let calendarContributions = 0;
+              data.forEach((day) => {
+                calendarContributions += day.count;
+              });
+              setTotalContributions1(calendarContributions);
+              return data;
+            }}
+            weekStart={1}
+            transformTotalCount
+            totalCount={totalContributions1 + totalContributions2}
+          />
+        </div>
+        <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 0 }}>
+          <GitHubCalendar
+            username='Pargeo82'
+            blockSize={18}
+            blockMargin={4}
+            weekStart={1}
+            theme={{
+              level4: '#39d353',
+              level3: '#26a641',
+              level2: '#006d32',
+              level1: '#0e4429',
+              level0: '#262626',
+            }}
+            transformData={(data) => {
+              let calendarContributions = 0;
+              data.forEach((day) => {
+                calendarContributions += day.count;
+              });
+              setTotalContributions2(calendarContributions);
+              return data;
+            }}
+            hideTotalCount={true}
+            hideColorLegend={true}
+          />
+        </div>
+      </Box>
+    </>
   );
 };
 
